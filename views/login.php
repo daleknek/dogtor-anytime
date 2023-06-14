7fc2b006f990825dfeae3638c3a09085c6828770
Nektaria <54439674+daleknek@users.noreply.github.com>
2023-06-14 10:33:53 +0300

    inline styling. removed from css file.

--- views/login.php
+++ views/login.php
@@ -1,30 +1,72 @@
-<div class="d-flex justify-content-end fixed-top m-2">
-    <a href="http://localhost/dogtor-anytime/index.php?controller=login" class="btn btn-primary ml-2">Login</a>
-    <a href="http://localhost/dogtor-anytime/index.php?controller=signup" class="btn btn-primary ml-2">Sign Up</a>
-</div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12 text-center mb-4">
-            <img src="images/logo-no-background.png" alt="Logo" class="logo">
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        html,
+        body {
+          height: 100%;
+        }
+
+        body {
+          display: flex;
+          align-items: center;
+          padding-top: 40px;
+          padding-bottom: 40px;
+          background-color: #f5f5f5;
+        }
+
+        .navbar {
+          padding-top: 10px;
+        }
+
+        .form {
+          width: 100%;
+          max-width: 330px;
+          padding: 15px;
+          margin: auto;
+        }
+
+        .form .checkbox {
+          font-weight: 400;
+        }
+
+        .form .form-floating:focus-within {
+          z-index: 2;
+        }
+
+        .form input[type="email"],
+        .form input[type="password"],
+        .form input[type="text"] {
+          margin-bottom: 10px;
+          border-radius: 5px;
+        }
+    </style>
+</head>
+<body style="background-image: url('images/pets.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+    <nav class="navbar fixed-top navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Dogtor Anytime</a>
         </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="text-center mb-4">
-                <h2>Login</h2>
+    </nav>
+    <main class="form text-center">
+        <form action="http://localhost/dogtor-anytime/index.php?route=login" method="post">
+            <h1 class="h3 mb-3 fw-normal">Login</h1>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+                <label for="floatingInput">Email</label>
             </div>
-            <form action="/login" method="post">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Username" name="uname" required>
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password" name="psw" required>
-                </div>
-                <div class="text-left mt-3">
-                    <a href="#" class="forgot-password-link">Forgot password?</a>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Login</button>
-            </form>
-        </div>
-    </div>
-</div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+    </main>
+</body>
+</html>
--- views/signup.php
+++ views/signup.php
@@ -1,44 +1,82 @@
-<div class="d-flex justify-content-end fixed-top m-2">
-    <a href="http://localhost/dogtor-anytime/index.php?controller=login" class="btn btn-primary ml-2">Login</a>
-    <a href="http://localhost/dogtor-anytime/index.php?controller=signup" class="btn btn-primary ml-2">Sign Up</a>
-</div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12 text-center mb-4">
-            <img src="images/logo-no-background.png" alt="Logo" class="logo">
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="text-center mb-4">
-                <h2>Sign Up</h2>
-            </div>
-            <form action="/signup" method="post">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Name" name="name" required>
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Surname" name="surname" required>
-                </div>
-                <div class="form-group">
-                    <input type="email" class="form-control" placeholder="Email" name="email" required>
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password" name="password" required>
-                </div>
-                <div class="form-row justify-content-center">
-                    <div class="form-group form-check col-md-5 text-center">
-                        <input type="radio" class="form-check-input" id="checkPatient" name="UserType"value="Patient">
-                        <label class="form-check-label" for="checkPatient">Patient</label>
-                    </div>
-                    <div class="form-group form-check col-md-5 text-center">
-                        <input type="radio" class="form-check-input" id="checkVet" name="UserType" value="Vet">
-                        <label class="form-check-label" for="checkVet">Vet</label>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
-            </form>
-        </div>
-    </div>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        html,
+        body {
+          height: 100%;
+        }
 
+        body {
+          display: flex;
+          align-items: center;
+          padding-top: 40px;
+          padding-bottom: 40px;
+          background-color: #f5f5f5;
+        }
+
+        .navbar {
+          padding-top: 10px;
+        }
+
+        .form {
+          width: 100%;
+          max-width: 330px;
+          padding: 15px;
+          margin: auto;
+        }
+
+        .form .checkbox {
+          font-weight: 400;
+        }
+
+        .form .form-floating:focus-within {
+          z-index: 2;
+        }
+
+        .form input[type="email"],
+        .form input[type="password"],
+        .form input[type="text"] {
+          margin-bottom: 10px;
+          border-radius: 5px;
+        }
+    </style>
+</head>
+<body style="background-image: url('images/pets.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+    <nav class="navbar fixed-top navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Dogtor Anytime</a>
+        </div>
+    </nav>
+    <main class="form text-center">
+        <form action="http://localhost/dogtor-anytime/index.php?route=login" method="post">
+            <h1 class="h3 mb-3 fw-normal">Sign Up</h1>
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingInput" placeholder="Name" required>
+                <label for="floatingInput">Name</label>
+            </div>
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingInput" placeholder="Surname" required>
+                <label for="floatingPassword">Surname</label>
+            </div>
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Email"required>
+                <label for="floatingPassword">Email</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div class="container mb-3 form-check form-check-inline">
+                <label>
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="vet" required> Vet
+                </label>
+                <label>
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="patient" required> Patient
+                </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+        </form>
+    </main>
+    </body>
+</html>
