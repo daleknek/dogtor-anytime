37e5c59789791314efbfd021cc5895942053295d
LydiaMic <129009692+LydiaMic@users.noreply.github.com>
2023-06-18 12:19:36 +0300

    fetch appointments

--- views/signup.php
+++ views/signup.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -65,8 +62,7 @@
           border-radius: 5px;
         }
     </style>
-</head>
-<body>
+
 
 <?php
 $servername = "localhost";
@@ -170,6 +166,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   </footer>
 
   <script src="js/bootstrap.bundle.min.js"></script>
-</body>
-    
-</html>
+
